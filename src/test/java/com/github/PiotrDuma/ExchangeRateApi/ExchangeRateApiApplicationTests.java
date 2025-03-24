b42067871fc8b37dfc2fd43d8e37bd56c8793ab5
PiotrDuma <piotrduma2014@gmail.com>
2025-03-24 14:33:11 +0100

    init project

---
+++ src/test/java/com/github/PiotrDuma/ExchangeRateApi/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PiotrDuma.ExchangeRateApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
