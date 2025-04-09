b663dd8d5b6b7130669452cfefd84f5900d0ae5e
PiotrDuma <piotrduma2014@gmail.com>
2025-04-09 20:07:24 +0200

    add flyway to project

---
+++ src/main/resources/db/migration/h2/V1__init-currencies-schema.sql
@@ -0,0 +1,29 @@
+    DROP TABLE if EXISTS currencies CASCADE;
+
+    DROP TABLE if EXISTS exchange_currencies CASCADE;
+
+    DROP TABLE if EXISTS rates CASCADE;
+
+    CREATE TABLE currencies (
+          id uuid NOT NULL,
+          version integer,
+          created timestamp(6) with time zone,
+          updated timestamp(6) with time zone,
+          base_currency varchar(3) NOT NULL UNIQUE,
+          converted_sum float(53),
+          primary key (id)
+    );
+
+    create table exchange_currencies (
+        currency_id uuid not null,
+        exchange_currencies varchar(3),
+        foreign key (currency_id) references currencies
+    );
+
+    create table rates (
+        currency_type varchar(3) not null,
+        rate float(53),
+        currency_id uuid not null,
+        primary key (currency_id, currency_type),
+        foreign key (currency_id) references currencies
+    );
